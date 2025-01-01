c04afc2d2eb59ba15c28c96b105c245b0c484d15
Charan23-sudo <ayom55054@gmail.com>
2025-01-01 21:39:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "CHARANKILLER"
 
-global.banknumber = "8133729715"
+global.banknumber = "09113415286"
 
-global.bankowner = "FADARE"
+global.bankowner = "CHRKLL"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
